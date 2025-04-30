040a5bbb132e09624f52ac3d914382e53f27cf1f
kirillkychkin <kychkin.kirill@gmail.com>
2025-04-30 12:23:09 +0900

    add or edit departments

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 12);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Создать";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(403, 174);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 52);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Показать";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(548, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(252, 450);
+            this.listBox1.TabIndex = 3;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(239, 14);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(239, 20);
+            this.txtDepartment.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Отдел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Должность";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Возраст";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(239, 49);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(239, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(239, 93);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(239, 20);
+            this.txtPosition.TabIndex = 10;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(239, 126);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(239, 20);
+            this.txtAge.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.TextBox txtAge;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
 
 namespace dotnet_lab_13
 {
@@ -15,6 +18,113 @@ namespace dotnet_lab_13
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string filePath = "people.xml";
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(decl);
+
+            XmlElement company = doc.CreateElement("Company");
+            doc.AppendChild(company);
+
+            doc.Save(filePath);
+            MessageBox.Show("XML-файл создан.");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Сначала создайте XML-файл.");
+                return;
+            }
+
+            string nameText = txtName.Text;
+            string ageText = txtAge.Text;
+            string positionText = txtPosition.Text;
+            string departmentText = txtDepartment.Text;
+
+            if (nameText == "" || ageText == "" || positionText == "" || departmentText == "")
+            {
+                MessageBox.Show("Заполните все поля");
+                return;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            string deptName = txtDepartment.Text.Trim();
+            XmlElement company = doc.DocumentElement;
+
+            XmlElement department = null;
+
+            // Проверка: существует ли уже такой отдел
+            foreach (XmlElement dept in company.GetElementsByTagName("Department"))
+            {
+                if (dept.GetAttribute("name") == deptName)
+                {
+                    department = dept;
+                    break;
+                }
+            }
+
+            // Если нет — создаем
+            if (department == null)
+            {
+                department = doc.CreateElement("Department");
+                department.SetAttribute("name", deptName);
+                company.AppendChild(department);
+            }
+
+            XmlElement person = doc.CreateElement("Person");
+
+            XmlElement name = doc.CreateElement("Name");
+            name.InnerText = nameText;
+            person.AppendChild(name);
+
+            XmlElement age = doc.CreateElement("Age");
+            age.InnerText = ageText;
+            person.AppendChild(age);
+
+            XmlElement position = doc.CreateElement("Position");
+            position.InnerText = positionText;
+            person.AppendChild(position);
+
+            department.AppendChild(person);
+            doc.Save(filePath);
+
+            MessageBox.Show("Сотрудник добавлен.");
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Файл не найден.");
+                return;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            listBox1.Items.Clear();
+
+            foreach (XmlNode dept in doc.SelectNodes("//Department"))
+            {
+                string deptName = ((XmlElement)dept).GetAttribute("name");
+
+                foreach (XmlNode person in dept.SelectNodes("Person"))
+                {
+                    string name = person["Name"]?.InnerText ?? "";
+                    string age = person["Age"]?.InnerText ?? "";
+                    string position = person["Position"]?.InnerText ?? "";
+
+                    listBox1.Items.Add($"{deptName}: {name}, {age} лет, {position}");
+                }
+            }
         }
     }
 }
